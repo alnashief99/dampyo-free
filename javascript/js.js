953a7c06107aa3b484f184f1c10b3e10169a9e86
alnashief99 <79649241+alnashief99@users.noreply.github.com>
2023-01-28 18:40:29 +0800

    Update js.js

--- javascript/js.js
+++ javascript/js.js
@@ -6,81 +6,58 @@ function preventNonNumericalInput(e) {
   if (!charStr.match(/^[0-9]+$/)) e.preventDefault();
 }
 
-let quantity1 = document.querySelector(".quantity1");
-let quantity11 = Number(quantity1.value);
-let glitter1 = document.querySelector(".glitter1");
-let darksteel1 = document.querySelector(".darksteel1");
-let copper1 = document.querySelector(".copper1");
-
-let quantity2 = document.querySelector(".quantity2");
-let quantity22 = Number(quantity1.value);
-let glitter2 = document.querySelector(".glitter2");
-let darkstee22 = document.querySelector(".darksteel2");
-let copper2 = document.querySelector(".copper2");
-
-let quantity3 = document.querySelector(".quantity3");
-let quantity33 = Number(quantity1.value);
-let glitter3 = document.querySelector(".glitter3");
-let darkstee33 = document.querySelector(".darksteel3");
-let copper3 = document.querySelector(".copper3");
+let inputFields = [
+  {
+    quantity: document.querySelector(".quantity1"),
+    glitter: document.querySelector(".glitter1"),
+    darksteel: document.querySelector(".darksteel1"),
+    copper: document.querySelector(".copper1"),
+  },
+  {
+    quantity: document.querySelector(".quantity2"),
+    glitter: document.querySelector(".glitter2"),
+    darksteel: document.querySelector(".darksteel2"),
+    copper: document.querySelector(".copper2"),
+  },
+  {
+    quantity: document.querySelector(".quantity3"),
+    glitter: document.querySelector(".glitter3"),
+    darksteel: document.querySelector(".darksteel3"),
+    copper: document.querySelector(".copper3"),
+  },
+];
 
 document.querySelector("#compute").addEventListener("click", function () {
-  let rarity1 = document.querySelector("#rarity").value;
-  if (rarity1 == "Epic") {
-    quantity11 = Number(quantity1.value);
-    glitter1.innerHTML = quantity11 * 25;
-    copper1.innerHTML = quantity11 * 20000;
-    darksteel1.innerHTML = quantity11 * 5000;
-    quantity22 = Number(quantity2.value);
-    glitter2.innerHTML = quantity22 * 25;
-    copper2.innerHTML = quantity22 * 20000;
-    darkstee22.innerHTML = quantity22 * 5000;
-    quantity33 = Number(quantity3.value);
-    glitter3.innerHTML = quantity33 * 25;
-    copper3.innerHTML = quantity33 * 20000;
-    darkstee33.innerHTML = quantity33 * 5000;
-  } else if (rarity1 == "Legendary") {
-    quantity11 = Number(quantity1.value);
-    glitter1.innerHTML = quantity11 * 125;
-    copper1.innerHTML = quantity11 * 100000;
-    darksteel1.innerHTML = quantity11 * 25000;
-    quantity22 = Number(quantity2.value);
-    glitter2.innerHTML = quantity22 * 125;
-    copper2.innerHTML = quantity22 * 100000;
-    darkstee22.innerHTML = quantity22 * 25000;
-    quantity33 = Number(quantity3.value);
-    glitter3.innerHTML = quantity33 * 125;
-    copper3.innerHTML = quantity33 * 100000;
-    darkstee33.innerHTML = quantity33 * 25000;
+  let rarity = document.querySelector("#rarity").value;
+  let glitterMultiplier, darksteelMultiplier, copperMultiplier;
+  if (rarity === "Epic") {
+    glitterMultiplier = 25;
+    darksteelMultiplier = 5000;
+    copperMultiplier = 20000;
+  } else if (rarity === "Legendary") {
+    glitterMultiplier = 125;
+    darksteelMultiplier = 25000;
+    copperMultiplier = 100000;
   }
-});
 
-let totalGlitter = document.querySelector(".totalglitter").innerHTML;
-let totalDarksteel = document.querySelector(".totaldarksteel").innerHTML;
-let totalCopper = document.querySelector(".totalcopper").innerHTML;
+  let totalGlitter = 0;
+  let totalDarksteel = 0;
+  let totalCopper = 0;
 
-document.querySelector("#compute").addEventListener("click", function () {
-  let overglit = (document.querySelector(".totalglitter").innerHTML =
-    Number(glitter1.innerHTML) +
-    Number(glitter2.innerHTML) +
-    Number(glitter3.innerHTML));
+  inputFields.forEach((field) => {
+    let quantity = Number(field.quantity.value);
+    field.glitter.innerHTML = quantity * glitterMultiplier;
+    field.darksteel.innerHTML = quantity * darksteelMultiplier;
+    field.copper.innerHTML = quantity * copperMultiplier;
+    totalGlitter += Number(field.glitter.innerHTML);
+    totalDarksteel += Number(field.darksteel.innerHTML);
+    totalCopper += Number(field.copper.innerHTML);
+  });
 
   document.querySelector(".totalglitter").innerHTML =
-    overglit.toLocaleString("en-US");
-
-  let overds = (document.querySelector(".totaldarksteel").innerHTML =
-    Number(darksteel1.innerHTML) +
-    Number(darkstee22.innerHTML) +
-    Number(darkstee33.innerHTML));
-
+    totalGlitter.toLocaleString("en-US");
   document.querySelector(".totaldarksteel").innerHTML =
-    overds.toLocaleString("en-US");
-
-  let overcopper = (document.querySelector(".totalcopper").innerHTML =
-    Number(copper1.innerHTML) +
-    Number(copper2.innerHTML) +
-    Number(copper3.innerHTML));
-
+    totalDarksteel.toLocaleString("en-US");
   document.querySelector(".totalcopper").innerHTML =
-    overcopper.toLocaleString("en-US");
+    totalCopper.toLocaleString("en-US");
 });
